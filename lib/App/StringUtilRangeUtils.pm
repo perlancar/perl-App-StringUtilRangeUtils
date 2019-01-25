b9b07c2416a735d709bd541c40e6f3ac4833104b
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-25 18:22:26 +0700

    Initial commit

---
+++ lib/App/StringUtilRangeUtils.pm
@@ -0,0 +1,18 @@
+package App::StringUtilRangeUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLIs for String::Util::Range
+
+=head1 DESCRIPTION
+
+This distribution includes the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<String::Util::Range>
